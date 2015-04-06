a86d9892940729881cddcbd204a69ca9d46589f8
Ingmar Steen <iksteen@gmail.com>
2015-04-06 19:57:55 +0200

    Re-add background moving code.

--- themes/dino/static/scripts.js
+++ themes/dino/static/scripts.js
@@ -7,5 +7,11 @@
         $('.menu-button').click(function() {
             $('aside').toggleClass('collapsed');
         });
+
+        $(document).mousemove(function(event) {
+            var offset = 100 - (event.pageX / $(window).width()) * 100;
+            $('.img-front').css('background-position-x', '-' + (offset / 10) + 'px');
+            $('.img-middle').css('background-position-x', '-' + (offset / 15) + 'px');
+        });
     });
 })(jQuery, this);
