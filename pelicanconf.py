51b1d57f2d1ac86df3db1290fb666054378525b2
Ingmar Steen <iksteen@gmail.com>
2015-04-08 12:05:21 +0200

    Release the dino!

--- pelicanconf.py
+++ pelicanconf.py
@@ -48,3 +48,5 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives', 'dino']
